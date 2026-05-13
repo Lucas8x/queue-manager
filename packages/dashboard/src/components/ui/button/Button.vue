de568e7a99c379e5e0b04282f6578745e232babf
lucas8x <lucasemanuel8x@gmail.com>
2026-05-12 21:00:22 -0300

    style: add cursor pointer to ui/button

--- packages/dashboard/src/components/ui/button/Button.vue
+++ packages/dashboard/src/components/ui/button/Button.vue
@@ -22,7 +22,7 @@ const props = withDefaults(defineProps<Props>(), {
     data-slot="button"
     :as="as"
     :as-child="asChild"
-    :class="cn(buttonVariants({ variant, size }), props.class)"
+    :class="cn(buttonVariants({ variant, size }), 'not-disabled:cursor-pointer', props.class)"
   >
     <slot />
   </Primitive>
